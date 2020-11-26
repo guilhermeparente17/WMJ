00a50ffe682a2dd6c9ef83517ff5cd240181f511
guilherme <guilherme.parente@outlook.com>
2020-11-25 22:13:10 -0300

    EDitando o Sidebar

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { Link as LinkS} from 'react-scroll'
 
 export const SidebarContainer = styled.div`
     display: grid;
@@ -24,6 +25,62 @@ export const Icon = styled.div`
     cursor: pointer;
 `
 
+export const SidebarWrapper = styled.div`
+    color: #FFF;
+`
+
+export const SidebarMenu = styled.ul`
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(6, 80px);
+    text-align:center;
+
+    @media screen and (max-width: 480px){
+        grid-template-rows: repeat(6, 100px);
+    }
+`   
+
+export const SidebarLink = styled(LinkS)`
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    transition: 0.2s ease-in-out;
+    color: #fff;
+    cursor: pointer;
+
+    &:hover{
+        color:#E8D4D1;
+        transition: 0.4s ease-in-out;
+    }
+`
+
+export const SideBtnWrap = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+export const SidebarRoute = styled(LinkS)`
+    border-radius: 50px;
+    background:#008000;
+    white-space: nowrap;
+    padding: 16px 22px;
+    color: #FFF;
+    font-size: 16px;
+    outline: none;
+    border:none;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+    text-decoration: none;
+
+    &:hover{
+        transition: all 0.2s ease-in-out;
+        background: #008C00;
+    }
+`
+export const NavbarImage = styled.img`
+    width: 60px;
+    height: 60px;
+`
 
 
 
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
-import { SidebarContainer, 
-    Icon
+import { 
+    SidebarContainer, 
+    Icon,
+    SidebarWrapper,
+    SidebarMenu,
+    SidebarLink,
+    SidebarRoute,
+    NavbarImage
 } from './SidebarElements'
 import { FaTimes } from 'react-icons/fa'
 
@@ -11,6 +17,15 @@ const index = ({ isOpen, toggle }) => {
                 <Icon onClick={toggle}>
                     <FaTimes />
                 </Icon>
+                <SidebarWrapper>
+                    <SidebarMenu>
+                        <SidebarLink to="/"><NavbarImage src="https://i.ibb.co/0DXg0CV/logo1.png" /></SidebarLink>
+                        <SidebarLink to="/" onClick={toggle}>Serviços</SidebarLink>
+                        <SidebarLink to="/" onClick={toggle}>Sobre nós</SidebarLink>
+                        <SidebarLink to="/" onClick={toggle}>Contato</SidebarLink>
+                        <SidebarLink><SidebarRoute to="/">Faça um orçamento</SidebarRoute></SidebarLink>
+                    </SidebarMenu>
+                </SidebarWrapper>
             </SidebarContainer>
 
         </>
