3bb0cd67449072fc63548b851a7b092920234d17
guilherme <guilherme.parente@outlook.com>
2020-11-25 21:37:21 -0300

    Editando o Navbar

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -9,31 +9,37 @@ export const NavbarContainer = styled.div`
     padding: 15px;
     background: linear-gradient(to right, #006400, #083B05);
     z-index: 999;
+
+    @media screen and (max-width: 740px){
+        justify-content: space-between;
+    }
     
 
 `
 
 export const NavbarLink = styled(LinkS)`
     color: #fff;
-    font-size: 1.7rem;
+    font-size: 1.2rem;
     font-family: 'Lato', sans-serif;
     cursor: pointer;
+    transition: 0.2s ease-in-out;
 
     &:hover{
         color: #E8D4D1;
+        transition: 0.4s ease-in-out;
     }
 `
 
 export const NavbarImage = styled.img`
-    width: 80px;
-    height: 80px;
+    width: 60px;
+    height: 60px;
 `
 
 export const NavBarList = styled.ul`
     display: flex;
     margin-left: 100px;
 
-    @media screen and (max-width: 820px){
+    @media screen and (max-width: 740px){
         display: none;
     }
     
@@ -42,7 +48,7 @@ export const NavBarList = styled.ul`
 export const MobileIcon = styled.div`
 
 display: none;
-@media screen and (max-width: 820px){
+@media screen and (max-width: 740px){
         display: block;
         color: #fff;
         font-size: 2.5rem;
@@ -57,17 +63,24 @@ export const NavbarItem = styled.li`
 `
 
 export const Buttom = styled(LinkS)`
-    padding:14px 38px;
+    padding:14px 18px;
     background: #008000;
     border-radius:24px;
     color: #fff;
     cursor: pointer;
-    font-size: 1rem;
+    font-size: 0.9rem;
+    transition: 0.4s ease-in-out;
 
     &:hover{
-        background: #fff;
-        color: #000;
+        transition: 0.4s ease-in-out;
+        background: #008C00;
     }
+
+    @media screen and (max-width: 740px){
+        display: none;
+    }
+
+
 `
 
 export const NavButton = styled.div`
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -21,7 +21,7 @@ const Navbar = ({ toggle}) => {
                     <NavbarItem><NavbarLink to="/">Contato</NavbarLink></NavbarItem>
                 </NavBarList>
                 <NavButton>
-                    <Buttom to="/">Faça um orçamentossss</Buttom>
+                    <Buttom to="/">Faça um orçamento</Buttom>
                 </NavButton>
                 <MobileIcon onClick={toggle}>
                     <FaBars />
